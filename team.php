4d25cc39b3bb1e0150ff3975d9d7ff9da9cc404e
theo declerck <theo.declerck@epita.com>
2022-02-12 17:06:36 +0100

    init commit

---
+++ team.php
@@ -0,0 +1,46 @@
+<?php error_reporting(E_ERROR | E_PARSE);
+?>
+
+<html lang="">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>Team</title>
+</head>
+<body>
+<div class="container">
+    <h1 style="flex: auto">Nom team</h1>
+    <h2>Equipiers</h2>
+    <div class="theo">
+        <ul class ="list-group">
+            <li class="list-group-item"><p>Declerck</p></li>
+            <li class="list-group-item"><p>Theo</p></li>
+            <li class="list-group-item"><a href="mailto:theo.declerck@epita.fr">theo.declerck@epita.fr</a></li>
+        </ul>
+    </div>
+    <br>
+    <div class="romain">
+        <ul class ="list-group">
+            <li class="list-group-item"><p>Gauvreau</p></li>
+            <li class="list-group-item"><p>Romain</p></li>
+            <li class="list-group-item"><a href="mailto:romain.gauvreau@epita.fr">romain.gauvreau@epita.fr</a></li>
+        </ul>
+    </div>
+    <br>
+    <div class="abdel">
+        <ul class ="list-group">
+            <li class="list-group-item"><p>Mansouri</p></li>
+            <li class="list-group-item"><p>Abdel</p></li>
+            <li class="list-group-item"><a href="mailto:abdelaziz.mansouri@epita.fr">abdelaziz.mansouri@epita.fr</a></li>
+        </ul>
+    </div>
+    <br>
+    <div class="valentin">
+        <ul class ="list-group">
+            <li class="list-group-item"><p>Dumousset</p></li>
+            <li class="list-group-item"><p>Valentin</p></li>
+            <li class="list-group-item"><a href="mailto:valentin.dumousset@epita.fr">valentin.dumousset@epita.fr</a></li>
+        </ul>
+    </div>
+</div>
+</body>
